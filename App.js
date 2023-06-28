ad013ae9123738b55e4da932a733296132c8d13e
rupeshgaur2001 <rupeshgaurr200@gmail.com>
2023-06-28 12:02:41 +0530

    Homework2

---
+++ App.js
@@ -0,0 +1,47 @@
+
+ // Addtion
+let para1=4
+let para2=6
+
+function Addition(){
+    console.log("Addition : ",para1+para2+para3+para4)
+
+}
+
+Addition(para1,para2,para3=5,para4=8)
+
+//Subtraction
+
+let para5=4
+let para6=6
+
+function Subtract(){
+    console.log("Subtraction : ",para5-para6-para7 -para8)
+
+}
+
+Subtract(para5,para6,para7=5,para8=8)
+
+//Mutliplication
+
+let para9=4
+let para10=6
+
+function Multiply(){
+    console.log("Multiplication : ",para9*para10*para11*para12)
+
+}
+
+Multiply(para9,para10,para11=5,para12=8)
+
+
+//Division
+
+let para13=4
+let para14=6
+function Division(){
+    console.log("division : ", para13/para14/para15/para16)
+
+}
+
+Division(para13,para14,para15=9,para16=8)
